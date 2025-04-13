8d5a5b127baac14f3261cebd3d457ca23b567b29
techleadpro <129011467+techleadpro@users.noreply.github.com>
2025-04-13 07:08:30 -0400

    Update drivers_controller.rb

--- backend/app/controllers/api/v1/drivers_controller.rb
+++ backend/app/controllers/api/v1/drivers_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
    class DriversController < ApplicationController
+     ALLOWED_DATA = %[full_name email phone truck_type truck_data].freeze
+     ## added changes
      # all drivers
      def index
        drivers = Driver.all
@@ -8,11 +10,13 @@ module Api
      end
 
   def show
-    render json: @driver
+    driver = Driver.find(params[:id])
+    render json: driver
   end
 
   def create
-    driver = Driver.new(driver_params)
+    data = json_payload.select{|k| ALLOWED_DATA.include?(k)}
+    driver = Driver.new(data)
     if driver.save
       render json: driver, status: :created
     else
@@ -21,30 +25,34 @@ module Api
   end
 
   def update
-    if @driver.update(driver_params)
-      render json: @driver
+    data = json_payload.select{|k| ALLOWED_DATA.include?(k)}
+    driver = Driver.find(params[:id])
+    if driver.update(data)
+      render json: driver
     else
-      render json: @driver.errors, status: :unprocessable_entity
+      render json: driver.errors, status: :unprocessable_entity
     end
   end
 
   def destroy
-    @driver.destroy
+    driver = Driver.find(params[:id])
+  #  @driver.destroy
+    driver.destroy
     head :no_content
   end
 
-  private
+#  private
 
-  def set_driver
-    @driver = driver.find_by(id: params[:id])
-    unless @driver
-      render json: { error: 'driver not found' }, status: :not_found
-    end
-  end
+#  def set_driver
+#    @driver = driver.find_by(id: params[:id])
+#    unless @driver
+#      render json: { error: 'driver not found' }, status: :not_found
+#    end
+#  end
 
-  def driver_params
-    params.require(:driver).permit(:full_name, :email, :phone, :truck_type, :truck_data)
-  end
+#  def driver_params
+#    params.require(:driver).permit(:full_name, :email, :phone, :truck_type, :truck_data)
+#  end
 
 end
 end
